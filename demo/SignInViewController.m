4b62fc8364485bd925401c3b307a258801b882af
Joel Oliveira <joeloliveira1979@gmail.com>
2017-01-31 19:28:06 +0100

    - added artwork, controller and view for create account

--- demo/SignInViewController.m
+++ demo/SignInViewController.m
@@ -21,7 +21,7 @@
 @property (nonatomic, strong) UITextField * emailField;
 @property (nonatomic, strong) UITextField * passwordField;
 @property (nonatomic, strong) FormButton * formButton;
-@property (nonatomic, strong) FormButton * lostPasswordButton;
+@property (nonatomic, strong) FormButton * signupButton;
 
 @end
 
@@ -40,6 +40,7 @@
     NSMutableArray * section1 = [NSMutableArray array];
     [section1 addObject:@{@"label":LS(@"email_label"), @"placeholder":LS(@"email_placeholder"), @"value":@""}];
     [section1 addObject:@{@"label":LS(@"password_label"), @"placeholder":LS(@"password_placeholder"), @"value":@""}];
+    [section1 addObject:@{@"label":LS(@"lost_password_button_text"), @"placeholder":@"", @"value":@""}];
     
     [[self navSections] addObject:section1];
     
@@ -100,7 +101,7 @@
     [[cell textLabel] setText:[item objectForKey:@"label"]];
     [[cell textLabel] setFont:LATO_FONT(14)];
     
-    if ([[item objectForKey:@"label"] isEqual:LS(@"signin_password_label")]) {
+    if ([[item objectForKey:@"label"] isEqual:LS(@"password_label")]) {
         [self setPasswordField:[[UITextField alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width / 2, 40)]];
         [[self passwordField] setDelegate:self];
         [[self passwordField] setText:@""];
@@ -110,6 +111,15 @@
         [[self passwordField] setTag:[indexPath row] + 100];
         [[self passwordField] setSecureTextEntry:YES];
         [cell setAccessoryView:[self passwordField]];
+    } else if ([[item objectForKey:@"label"] isEqual:LS(@"lost_password_button_text")]) {
+        
+        UILabel * label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width, 40)];
+        [label setText:LS(@"lost_password_button_text")];
+        [label setTextAlignment:NSTextAlignmentRight];
+        [label setFont:LATO_LIGHT_FONT(12)];
+        [label setTextColor:FACEBOOK_COLOR];
+        [cell setAccessoryView:label];
+        
     } else {
         [self setEmailField:[[UITextField alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width / 2, 40)]];
         [[self emailField] setDelegate:self];
@@ -122,10 +132,24 @@
         [cell setAccessoryView:[self emailField]];
     }
     
+    
     [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
     return cell;
 }
 
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    NSDictionary * item = (NSDictionary *)[[[self navSections] objectAtIndex:[indexPath section]] objectAtIndex:[indexPath row]];
+    
+    if ([[item objectForKey:@"label"] isEqual:LS(@"lost_password_button_text")]) {
+        
+        [self goToLostPassword];
+        
+    }
+    
+}
 
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
@@ -156,10 +180,10 @@
     [footerView addSubview:[self formButton]];
     
     
-    [self setLostPasswordButton:[[FormButton alloc] initWithFrame:CGRectMake(10,80,tableView.frame.size.width - 20, 60)  andText:LS(@"lost_password_button_text") andTextColor:[UIColor whiteColor] andBgColor:MAIN_COLOR]];
-    [[self lostPasswordButton] addTarget:self action:@selector(goToLostPassword) forControlEvents:UIControlEventTouchUpInside];
+    [self setSignupButton:[[FormButton alloc] initWithFrame:CGRectMake(10,80,tableView.frame.size.width - 20, 60)  andText:LS(@"signup_button_text") andTextColor:[UIColor whiteColor] andBgColor:MAIN_COLOR]];
+    [[self signupButton] addTarget:self action:@selector(goToSignUp) forControlEvents:UIControlEventTouchUpInside];
     
-    [footerView addSubview:[self lostPasswordButton]];
+    [footerView addSubview:[self signupButton]];
     
     return footerView;
     
@@ -303,6 +327,10 @@
 
 -(void)goToLostPassword{
     [self performSegueWithIdentifier:@"LostPassword" sender:self];
+}
+
+-(void)goToSignUp{
+    [self performSegueWithIdentifier:@"SignUp" sender:self];
 }
 
 - (void)didReceiveMemoryWarning {
--- demo/SignUpViewController.m
+++ demo/SignUpViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "SignUpViewController.h"
+#import "Definitions.h"
+#import "FormButton.h"
+#import "NotificarePushLib.h"
 
 @interface SignUpViewController ()
 
@@ -15,6 +18,11 @@
 @property (nonatomic, strong) IBOutlet UIView * loadingScreen;
 @property (nonatomic, strong) NSMutableArray * navSections;
 @property (nonatomic, strong) NSMutableArray * sectionTitles;
+@property (nonatomic, strong) UITextField * nameField;
+@property (nonatomic, strong) UITextField * emailField;
+@property (nonatomic, strong) UITextField * passwordField;
+@property (nonatomic, strong) UITextField * confirmPasswordField;
+@property (nonatomic, strong) FormButton * formButton;
 
 @end
 
@@ -22,22 +30,280 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    [self setTitle:LS(@"title_signup")];
+    
+    [self setNavSections:[NSMutableArray array]];
+    [self setSectionTitles:[NSMutableArray array]];
+    [[self sectionTitles] addObject:LS(@"section_signin_header")];
+    
+    
+    NSMutableArray * section1 = [NSMutableArray array];
+    [section1 addObject:@{@"label":LS(@"name_label"), @"placeholder":LS(@"name_placeholder"), @"value":@""}];
+    [section1 addObject:@{@"label":LS(@"email_label"), @"placeholder":LS(@"email_placeholder"), @"value":@""}];
+    [section1 addObject:@{@"label":LS(@"password_label"), @"placeholder":LS(@"password_placeholder"), @"value":@""}];
+    [section1 addObject:@{@"label":LS(@"confirm_password_label"), @"placeholder":LS(@"confirm_password_placeholder"), @"value":@""}];
+    
+    [[self navSections] addObject:section1];
+    
+    [[self tableView] reloadData];
+    
+    UIBarButtonItem * leftButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"back"] style:UIBarButtonItemStylePlain target:self action:@selector(back)];
+    [leftButton setTintColor:MAIN_COLOR];
+    [[self navigationItem] setLeftBarButtonItem:leftButton];
+    
+    [[self tableView] setBackgroundColor:WILD_SAND_COLOR];
+    [[self view] setBackgroundColor:WILD_SAND_COLOR];
 }
 
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    [[self navigationController] setNavigationBarHidden:NO];
+    
+}
+
+
+-(void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    
+}
+
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return [[self navSections] count];
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return [[[self navSections] objectAtIndex:section] count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    
+    
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"FormFieldCell"];
+    
+    if (cell == nil) {
+        
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"FormFieldCell"];
+        
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        
+    }
+    
+    NSDictionary * item = (NSDictionary *)[[[self navSections] objectAtIndex:[indexPath section]] objectAtIndex:[indexPath row]];
+    
+    
+    [[cell textLabel] setText:[item objectForKey:@"label"]];
+    [[cell textLabel] setFont:LATO_FONT(14)];
+    
+    if ([[item objectForKey:@"label"] isEqual:LS(@"password_label")]) {
+        [self setPasswordField:[[UITextField alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width / 2, 40)]];
+        [[self passwordField] setDelegate:self];
+        [[self passwordField] setText:@""];
+        [[self passwordField] setTextAlignment:NSTextAlignmentRight];
+        [[self passwordField] setFont:LATO_LIGHT_FONT(14)];
+        [[self passwordField] setPlaceholder:[item objectForKey:@"placeholder"]];
+        [[self passwordField] setTag:[indexPath row] + 100];
+        [[self passwordField] setSecureTextEntry:YES];
+        [cell setAccessoryView:[self passwordField]];
+    } else if ([[item objectForKey:@"label"] isEqual:LS(@"confirm_password_label")]) {
+        [self setConfirmPasswordField:[[UITextField alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width / 2, 40)]];
+        [[self confirmPasswordField] setDelegate:self];
+        [[self confirmPasswordField] setText:@""];
+        [[self confirmPasswordField] setTextAlignment:NSTextAlignmentRight];
+        [[self confirmPasswordField] setFont:LATO_LIGHT_FONT(14)];
+        [[self confirmPasswordField] setPlaceholder:[item objectForKey:@"placeholder"]];
+        [[self confirmPasswordField] setTag:[indexPath row] + 100];
+        [[self confirmPasswordField] setSecureTextEntry:YES];
+        [cell setAccessoryView:[self confirmPasswordField]];
+    } else if ([[item objectForKey:@"label"] isEqual:LS(@"name_label")]) {
+        [self setNameField:[[UITextField alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width / 2, 40)]];
+        [[self nameField] setDelegate:self];
+        [[self nameField] setText:@""];
+        [[self nameField] setTextAlignment:NSTextAlignmentRight];
+        [[self nameField] setFont:LATO_LIGHT_FONT(14)];
+        [[self nameField] setPlaceholder:[item objectForKey:@"placeholder"]];
+        [[self nameField] setTag:[indexPath row] + 100];
+        [[self nameField] setKeyboardType:UIKeyboardTypeDefault];
+        [cell setAccessoryView:[self nameField]];
+    }  else {
+        [self setEmailField:[[UITextField alloc] initWithFrame:CGRectMake(0, 0, tableView.frame.size.width / 2, 40)]];
+        [[self emailField] setDelegate:self];
+        [[self emailField] setText:@""];
+        [[self emailField] setTextAlignment:NSTextAlignmentRight];
+        [[self emailField] setFont:LATO_LIGHT_FONT(14)];
+        [[self emailField] setPlaceholder:[item objectForKey:@"placeholder"]];
+        [[self emailField] setTag:[indexPath row] + 100];
+        [[self emailField] setKeyboardType:UIKeyboardTypeEmailAddress];
+        [cell setAccessoryView:[self emailField]];
+    }
+    
+    [cell setSelectionStyle:UITableViewCellSelectionStyleNone];
+    return cell;
+}
+
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
+{
+    
+    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0,0,tableView.frame.size.width,SIGNIN_HEADER_HEIGHT)];
+    [headerView setBackgroundColor:WILD_SAND_COLOR];
+    
+    UIImageView * imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0,tableView.frame.size.width,SIGNIN_HEADER_HEIGHT)];
+    [imageView setImage:[UIImage imageNamed:@"account"]];
+    [imageView setContentMode:UIViewContentModeCenter];
+    
+    [headerView addSubview:imageView];
+    
+    return headerView;
+    
+}
+
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
+{
+    
+    UIView *footerView = [[UIView alloc] initWithFrame:CGRectMake(0,0,tableView.frame.size.width,SIGNIN_FOOTER_HEIGHT)];
+    [footerView setBackgroundColor:WILD_SAND_COLOR];
+    
+    [self setFormButton:[[FormButton alloc] initWithFrame:CGRectMake(10,10,tableView.frame.size.width - 20, 60)  andText:LS(@"signup_button_text") andTextColor:[UIColor whiteColor] andBgColor:MAIN_COLOR]];
+    [[self formButton] addTarget:self action:@selector(createAccount:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [footerView addSubview:[self formButton]];
+    
+    return footerView;
+    
+}
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    return FORMS_CELLHEIGHT;
+    
+}
+
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    
+    return SIGNIN_HEADER_HEIGHT;
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
+    
+    return SIGNIN_FOOTER_HEIGHT;
+    
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField*)textField;
+{
+    NSInteger nextTag = textField.tag + 1;
+    // Try to find next responder
+    UIResponder* nextResponder = [textField.superview viewWithTag:nextTag];
+    if (nextResponder) {
+        // Found next responder, so set it.
+        [nextResponder becomeFirstResponder];
+    } else {
+        // Not found, so remove keyboard.
+        [textField resignFirstResponder];
+    }
+    return NO;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    
+    
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    
+    [textField resignFirstResponder];
+    
+}
+
+-(void)createAccount:(id)sender{
+    
+    [[self formButton] setEnabled:NO];
+    
+    if (![[self emailField] text]) {
+        
+        [self presentAlertViewForForm:LS(@"error_create_account_invalid_email")];
+        [[self formButton] setEnabled:YES];
+        
+        
+    }else if (![[[self passwordField] text] isEqualToString:[[self confirmPasswordField] text]]) {
+        
+        [self presentAlertViewForForm:LS(@"error_create_account_passwords_match")];
+        [[self formButton] setEnabled:YES];
+        
+    }else if ([[[self confirmPasswordField] text] length] < 5) {
+
+        [self presentAlertViewForForm:LS(@"error_create_account_small_password")];
+        [[self formButton] setEnabled:YES];
+        
+    } else {
+        
+        [[NotificarePushLib shared] createAccount:[[self emailField] text]
+                                withName:[[self nameField] text]
+                             andPassword:[[self passwordField] text]
+                       completionHandler:^(NSDictionary *info) {
+                           
+                           [self presentAlertViewForForm:LS(@"success_create_account")];
+                           [[self formButton] setEnabled:YES];
+                           
+                           [[self emailField] setText:@""];
+                           [[self nameField] setText:@""];
+                           [[self passwordField] setText:@""];
+                           [[self confirmPasswordField] setText:@""];
+                           [[self navigationController] popViewControllerAnimated:YES];
+                           
+                       } errorHandler:^(NSError *error) {
+                           
+                           [self presentAlertViewForForm:LS(@"error_create_account")];
+                           [[self formButton] setEnabled:YES];
+                           
+                       }];
+        
+    }
+    
+}
+
+
+
+-(void)presentAlertViewForForm:(NSString *)message{
+    UIAlertController * alert=   [UIAlertController
+                                  alertControllerWithTitle: APP_NAME
+                                  message:message
+                                  preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* cancel = [UIAlertAction
+                             actionWithTitle:LS(@"ok")
+                             style:UIAlertActionStyleDefault
+                             handler:^(UIAlertAction * action){}];
+    [alert addAction:cancel];
+    
+    [self presentViewController:alert animated:YES completion:^{
+        
+    }];
+}
+
+
+-(void)back{
+    
+    [[self navigationController] popViewControllerAnimated:YES];
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
